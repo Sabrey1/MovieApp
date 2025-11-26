0a21085bc68b668413179c33d50dcc8d681108b0
Sabrey <limsabriey94@gmail.com>
2025-11-26 17:00:42 +0700

    Update

---
+++ Backend/resources/views/Body/BodyPage.blade.php
@@ -0,0 +1,26 @@
+<div class="p-5">
+    <label class="relative block w-full max-w-sm">
+  <!-- Icon -->
+  <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-400">
+    <i class="fa-solid fa-magnifying-glass"></i>
+  </span>
+
+  <!-- Input -->
+  <input
+    type="search"
+    placeholder="Search movies..."
+    required
+    class="w-full py-2 pl-10 pr-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-gray-700"
+  />
+</label>
+
+<div class="mt-5">
+    <button class="btn bg-gray-500 rounded-full p-4 text-white">All</button>
+    <button class="btn bg-gray-500 rounded-full p-4 text-white">Action</button>
+    <button class="btn bg-gray-500 rounded-full p-4 text-white">Sci-Fi</button>
+    <button class="btn bg-gray-500 rounded-full p-4 text-white">Crime</button>
+    <button class="btn bg-gray-500 rounded-full p-4 text-white">Drama</button>
+    <button class="btn bg-gray-500 rounded-full p-4 text-white">Adventure</button>
+    <button class="btn bg-gray-500 rounded-full p-4 text-white">Romance</button>
+</div>
+</div>
